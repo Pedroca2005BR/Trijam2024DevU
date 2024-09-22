d026352cdd587e83e8b9a4023372befdaf40c42f
G-Mirand <130694211+G-Mirand@users.noreply.github.com>
2024-09-22 16:55:48 -0300

    Buffs são ativados logo depois de pisar em uma caixa
    Não entendi muito bem o sistema de movimento, não consegui mexer nele

--- Assets/Pedro/WeakPoint.cs
+++ Assets/Pedro/WeakPoint.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class WeakPoint : MonoBehaviour
 {
+    public Buffs Buffs;
+    public PlayerMovement PlayerMovement;
+    private void Start()
+    {
+        Buffs = GameObject.FindObjectOfType<Buffs>();
+        PlayerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Buffs.buffativar();
             SpawnBox.instance.RespawnBox();
             Destroy(transform.parent.gameObject);
         }
